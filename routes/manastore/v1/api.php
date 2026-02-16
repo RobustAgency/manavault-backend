20eb609f29eb7e4ae5203dcbb2693e7231a66f11
Abdul Wahab <wshahid767@gmail.com>
2026-02-16 16:07:50 +0500

    refactor: Add spacing for improved readability in API route definitions

--- routes/manastore/v1/api.php
+++ routes/manastore/v1/api.php
@@ -5,9 +5,11 @@ use App\Http\Controllers\Api\ManaStore\V1\ProductController;
 use App\Http\Controllers\Api\ManaStore\V1\SaleOrderController;
 
 Route::prefix('/v1')->group(function () {
+
     Route::get('/status', function () {
         return response()->json(['status' => 'ManaStore API is operational']);
     });
+
     Route::middleware(['manastore.auth'])->group(function () {
         Route::prefix('/products')->group(function () {
             Route::get('', [ProductController::class, 'index']);
