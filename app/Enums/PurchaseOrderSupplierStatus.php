aac972de79a2aeae91f2694c2b7f848370cda084
Abdul Wahab <wshahid767@gmail.com>
2026-05-19 18:10:04 +0500

    feat: Remove PENDING_VOUCHERS status from PurchaseOrderSupplierStatus enum

--- app/Enums/PurchaseOrderSupplierStatus.php
+++ app/Enums/PurchaseOrderSupplierStatus.php
@@ -7,5 +7,4 @@ enum PurchaseOrderSupplierStatus: string
     case PROCESSING = 'processing';
     case COMPLETED = 'completed';
     case FAILED = 'failed';
-    case PENDING_VOUCHERS = 'pending_vouchers';
 }
