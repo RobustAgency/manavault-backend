e69e99ab891cd151700311f8bf459b0adce87c66
Abdul Wahab <wshahid767@gmail.com>
2026-06-11 17:39:18 +0500

    refactor: remove unused PurchaseOrderStatusService from Gift2Games integration classes

--- app/Integrations/Gift2GamesGbp.php
+++ app/Integrations/Gift2GamesGbp.php
@@ -5,7 +5,6 @@ namespace App\Integrations;
 use App\Actions\Gift2Games\CreateOrder;
 use App\Services\Voucher\VoucherCipherService;
 use App\Services\Gift2Games\SyncDigitalProducts;
-use App\Services\PurchaseOrder\PurchaseOrderStatusService;
 
 class Gift2GamesGbp extends Gift2Games
 {
@@ -13,14 +12,12 @@ class Gift2GamesGbp extends Gift2Games
         CreateOrder $createOrder,
         SyncDigitalProducts $syncDigitalProducts,
         VoucherCipherService $voucherCipherService,
-        PurchaseOrderStatusService $purchaseOrderStatusService,
     ) {
         parent::__construct(
             'gift-2-games-gbp',
             $createOrder,
             $syncDigitalProducts,
             $voucherCipherService,
-            $purchaseOrderStatusService,
         );
     }
 }
--- app/Integrations/Gift2GamesUsd.php
+++ app/Integrations/Gift2GamesUsd.php
@@ -5,7 +5,6 @@ namespace App\Integrations;
 use App\Actions\Gift2Games\CreateOrder;
 use App\Services\Voucher\VoucherCipherService;
 use App\Services\Gift2Games\SyncDigitalProducts;
-use App\Services\PurchaseOrder\PurchaseOrderStatusService;
 
 class Gift2GamesUsd extends Gift2Games
 {
@@ -13,14 +12,12 @@ class Gift2GamesUsd extends Gift2Games
         CreateOrder $createOrder,
         SyncDigitalProducts $syncDigitalProducts,
         VoucherCipherService $voucherCipherService,
-        PurchaseOrderStatusService $purchaseOrderStatusService,
     ) {
         parent::__construct(
             'gift2games',
             $createOrder,
             $syncDigitalProducts,
             $voucherCipherService,
-            $purchaseOrderStatusService,
         );
     }
 }
