166afe9dafae9e767bfc7c6a11ef3331969493b4
Abdul Wahab <wshahid767@gmail.com>
2026-06-15 12:17:03 +0500

    fix: g2g params

--- app/Integrations/Gift2GamesEur.php
+++ app/Integrations/Gift2GamesEur.php
@@ -5,7 +5,6 @@ namespace App\Integrations;
 use App\Actions\Gift2Games\CreateOrder;
 use App\Services\Voucher\VoucherCipherService;
 use App\Services\Gift2Games\SyncDigitalProducts;
-use App\Services\PurchaseOrder\PurchaseOrderStatusService;
 
 class Gift2GamesEur extends Gift2Games
 {
@@ -13,14 +12,12 @@ class Gift2GamesEur extends Gift2Games
         CreateOrder $createOrder,
         SyncDigitalProducts $syncDigitalProducts,
         VoucherCipherService $voucherCipherService,
-        PurchaseOrderStatusService $purchaseOrderStatusService,
     ) {
         parent::__construct(
             'gift-2-games-eur',
             $createOrder,
             $syncDigitalProducts,
             $voucherCipherService,
-            $purchaseOrderStatusService,
         );
     }
 }
